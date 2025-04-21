67a4042d27eb5f69ab49b3910750bd01f47a4c0b
Abbitech7 <tajua2982@gmail.com>
2025-04-21 15:16:19 +0300

    some style modification and database connetivity

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,11 @@
 const mysql = require('mysql2/promise');
 
-// Create a connection pool
 const db = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '',  // Adjust password as necessary
+    password: '', 
     database: 'esma_tech_hub',
     port: 3306
 });
 
-// Export the pool, using promises for cleaner query handling
 module.exports = db;
